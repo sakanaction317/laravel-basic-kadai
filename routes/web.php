0939b989894d3d2b0a3a23b1ace3a5dd30ae7754
sakanaction317 <danki.karate9602@gmail.com>
2024-10-15 14:33:42 +0900

    kadaiを修正

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controller\PostController;
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
